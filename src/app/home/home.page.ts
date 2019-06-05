2220751fc0d06d3681bc87ebf33658a7991ed40b
adujoseph <iamjosephadu@gmail.com>
2019-06-05 16:12:35 +0100

    tried safari view controller but failed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafariViewController } from '@ionic-native/safari-view-controller/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private safariViewController: SafariViewController) {}
+
+  openweb(){
+    alert('pressed');
+    this.safariViewController.isAvailable().then((available: boolean) => {
+      if (available) {
+          this.safariViewController.show({
+            url: 'http://www.tnpsc.gov.in/',
+            hidden: false,
+            animated: false,
+            transition: 'curl',
+            enterReaderModeIfAvailable: true,
+            tintColor: '#ff0000'
+            })
+            // .then((result: any) => {
+            //       if(result.event === 'opened') console.log('Opened');
+            //       else if(result.event === 'loaded') console.log('Loaded');
+            //       else if(result.event === 'closed') console.log('Closed');
+            //    },(error: any) => console.error(error)
+            //   );
+          } else {
+      // use fallback browser, example InAppBrowser
+      }
+      }
+      );
+}
 
 }
